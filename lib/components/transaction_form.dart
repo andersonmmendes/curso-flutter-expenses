cdb5c86f83ba3069984363ca10bf55d0e1be121e
Anderson Mendes <developer.andersonmendes@gmail.com>
2022-06-08 23:43:41 -0300

    Aula 149 - Finalizada

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -46,56 +46,63 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(
-                labelText: 'Título',
-              ),
-              onSubmitted: (_) => _submitForm(),
-            ),
-            TextField(
-              controller: _valueController,
-              decoration: InputDecoration(
-                labelText: 'Valor (R\$)',
-              ),
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitForm(),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(_selectedDate == null
-                        ? 'Nenhuma data selecionada'
-                        : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}'),
-                  ),
-                  ElevatedButton(
-                    onPressed: _showDatePicker,
-                    child: Text('Seleciona Data'),
-                  )
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text('Nova Transação'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.purple,
-                  ),
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: 10 + MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: 'Título',
                 ),
-              ],
-            )
-          ],
+                onSubmitted: (_) => _submitForm(),
+              ),
+              TextField(
+                controller: _valueController,
+                decoration: InputDecoration(
+                  labelText: 'Valor (R\$)',
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitForm(),
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(_selectedDate == null
+                          ? 'Nenhuma data selecionada'
+                          : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _showDatePicker,
+                      child: Text('Seleciona Data'),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: Text('Nova Transação'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.purple,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
