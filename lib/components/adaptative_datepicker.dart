aefcf3783a840811a08f9270c43aa5c8d99d7b77
Anderson Mendes <developer.andersonmendes@gmail.com>
2022-06-13 22:00:01 -0300

    Aula 160 - Finalizada

---
+++ lib/components/adaptative_datepicker.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AdaptativeDatePicker extends StatelessWidget {
+  final DateTime selectedDate;
+  final Function(DateTime) onDateChanged;
+
+  const AdaptativeDatePicker({required this.selectedDate, required this.onDateChanged});
+
+  _showDatePicker(BuildContext context) {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2022),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      onDateChanged(pickedDate);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS ? Container(
+      height: 180,
+      child: CupertinoDatePicker(
+        mode: CupertinoDatePickerMode.date,
+        initialDateTime: DateTime.now(),
+        minimumDate: DateTime(2022),
+        maximumDate: DateTime.now(),
+        onDateTimeChanged: onDateChanged,
+      ),
+    ) : Container(
+      height: 70,
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(selectedDate == null
+                ? 'Nenhuma data selecionada'
+                : 'Data selecionada: ${DateFormat('dd/MM/y').format(selectedDate)}'),
+          ),
+          TextButton(
+            onPressed: () =>  _showDatePicker(context),
+            child: Text('Seleciona Data'),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,4 +1,5 @@
 import 'package:expenses/components/adaptative_button.dart';
+import 'package:expenses/components/adaptative_datepicker.dart';
 import 'package:expenses/components/adaptative_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -29,23 +30,6 @@ class _TransactionFormState extends State<TransactionForm> {
     widget.onSubmit(title, value, _selectedDate);
   }
 
-  _showDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2022),
-      lastDate: DateTime.now(),
-    ).then((pickedDate) {
-      if (pickedDate == null) {
-        return;
-      }
-
-      setState(() {
-        _selectedDate = pickedDate;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -71,21 +55,11 @@ class _TransactionFormState extends State<TransactionForm> {
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
                 onSubmitted: (_) => _submitForm(),
               ),
-              Container(
-                height: 70,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(_selectedDate == null
-                          ? 'Nenhuma data selecionada'
-                          : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}'),
-                    ),
-                    TextButton(
-                      onPressed: _showDatePicker,
-                      child: Text('Seleciona Data'),
-                    )
-                  ],
-                ),
+              AdaptativeDatePicker(
+                selectedDate: _selectedDate,
+                onDateChanged: (newDate) {
+                  _selectedDate = newDate;
+                },
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
